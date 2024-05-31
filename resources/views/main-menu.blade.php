0877b9761f9dd9c06e3510a1e02a02954267db4d
hokushinnitta <hokushinnitta@gmail.com>
2024-05-31 16:41:34 +0900

    Update main-menu.blade.php
    出荷処理等追加

--- resources/views/main-menu.blade.php
+++ resources/views/main-menu.blade.php
@@ -179,13 +179,27 @@
                             <input type="text" class="form-control" id="exampleInput1">
                         </div>
 
-                    <div class="mb-3">
-                        <label for="exampleInput3" class="form-label">Example input 7</label>
-                        <input type="text" class="form-control" id="exampleInput3">
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInput4" class="form-label">Example input 8</label>
-                        <input type="text" class="form-control" id="exampleInput4">
+                        <div class="row">
+                        <div class="col-md-2">
+                            <label for="exampleInput3" class="form-label">生産日</label>
+                            <input type="text" class="form-control" id="exampleInput3">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="exampleInput4" class="form-label">生産数量本社</label>
+                            <input type="text" class="form-control" id="exampleInput4">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="exampleInput4" class="form-label">生産数量第二工場</label>
+                            <input type="text" class="form-control" id="exampleInput4">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="exampleInput4" class="form-label">生産数量倉庫</label>
+                            <input type="text" class="form-control" id="exampleInput4">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="exampleInput4" class="form-label">合計生産数量</label>
+                            <input type="text" class="form-control" id="exampleInput4">
+                        </div>
                     </div>
                     <!-- More inputs as needed -->
                 </form>
@@ -193,19 +207,32 @@
             <div class="tab-pane fade" id="tab5" role="tabpanel" aria-labelledby="tab5-tab">
                 <!-- Content for Tab 2 -->
                 <form>
-                    
                         <div class="col-md-2">
                             <label for="exampleInput1" class="form-label">データNo</label>
                             <input type="text" class="form-control" id="exampleInput1">
                         </div>
 
-                    <div class="mb-3">
-                        <label for="exampleInput3" class="form-label">出荷日</label>
-                        <input type="text" class="form-control" id="exampleInput3">
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInput4" class="form-label">出荷数</label>
-                        <input type="text" class="form-control" id="exampleInput4">
+                        <div class="row">
+                        <div class="col-md-2">
+                            <label for="exampleInput3" class="form-label">出荷日</label>
+                            <input type="text" class="form-control" id="exampleInput3">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="exampleInput4" class="form-label">出荷数量本社</label>
+                            <input type="text" class="form-control" id="exampleInput4">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="exampleInput4" class="form-label">出荷数量第二工場</label>
+                            <input type="text" class="form-control" id="exampleInput4">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="exampleInput4" class="form-label">出荷数量倉庫</label>
+                            <input type="text" class="form-control" id="exampleInput4">
+                        </div>
+                        <div class="col-md-2">
+                            <label for="exampleInput4" class="form-label">合計出荷数量</label>
+                            <input type="text" class="form-control" id="exampleInput4">
+                        </div>
                     </div>
                     <!-- More inputs as needed -->
                 </form>
